a145aef8ca7ee6c7dbfd3bd5429ab96171db58e9
Garvit <122175234+garvit-exe@users.noreply.github.com>
2024-02-29 03:01:19 +0530

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -23,43 +23,38 @@ class AstralBody:
     G = 6.67428e-11
     SCALE = 250 / astronomical_unit
     TIMESTEP = 60 * 60 * 24
+    MAX_TRAIL_LENGTH = 250  # Maximum number of points in the trail
 
-    def __init__(self, x, y, radius, color, mass):
+    def __init__(self, x, y, z, radius, color, mass):
         self.x = x
         self.y = y
+        self.z = z
         self.radius = radius
         self.color = color
         self.mass = mass
 
-        self.orbit = []
+        self.trail = []
         self.sun = False
         self.distance_from_sun = 0
 
         self.vx = 0
         self.vy = 0
+        self.vz = 0
 
     def draw(self, win):
-        x = self.x * self.SCALE + width / 2
-        y = self.y * self.SCALE + height / 2
+        scale_factor = 1 - self.z / (10 * AstralBody.astronomical_unit)
+        scaled_radius = int(self.radius * scale_factor)
+        scaled_x = self.x * AstralBody.SCALE * scale_factor + width / 2
+        scaled_y = self.y * AstralBody.SCALE * scale_factor + height / 2
 
-        if len(self.orbit) > 2:
-            updated_coordinates = []
-
-            for coordinate in self.orbit:
-                x, y = coordinate
-                x = x * self.SCALE + width / 2
-                y = y * self.SCALE + height / 2
-                updated_coordinates.append((x, y))
-
-            pygame.draw.lines(win, self.color, False, updated_coordinates, 2)
-
-        pygame.draw.circle(win, self.color, (x, y), self.radius)
+        pygame.draw.circle(win, self.color, (int(scaled_x), int(scaled_y)), scaled_radius)
 
     def gravity(self, other):
-        other_x, other_y = other.x, other.y
+        other_x, other_y, other_z = other.x, other.y, other.z
         dist_x = other_x - self.x
         dist_y = other_y - self.y
-        distance = math.sqrt(dist_x ** 2 + dist_y ** 2)
+        dist_z = other_z - self.z
+        distance = math.sqrt(dist_x ** 2 + dist_y ** 2 + dist_z ** 2)
 
         if other.sun:
             self.distance_from_sun = distance
@@ -71,7 +66,7 @@ class AstralBody:
         return force_x, force_y
 
     def update_position(self, bodies):
-        totalForce_x = totalForce_y = 0
+        totalForce_x = totalForce_y = totalForce_z = 0
         for body in bodies:
             if self == body:
                 continue
@@ -85,26 +80,39 @@ class AstralBody:
 
         self.x += self.vx * self.TIMESTEP
         self.y += self.vy * self.TIMESTEP
-        self.orbit.append((self.x, self.y))
 
+        # Add current position to the trail
+        self.trail.append((self.x, self.y))
+        # Adjust the trail length based on distance from the sun
+        self.trail = self.trail[-min(int(self.distance_from_sun / 1e9), AstralBody.MAX_TRAIL_LENGTH):]
+
+    def draw_trail(self, win):
+        alpha_step = 255 / len(self.trail)
+        for i, (x, y) in enumerate(self.trail):
+            scale_factor = 1 - self.z / (10 * AstralBody.astronomical_unit)
+            scaled_x = x * AstralBody.SCALE * scale_factor + width / 2
+            scaled_y = y * AstralBody.SCALE * scale_factor + height / 2
+            alpha = 255 - i * alpha_step
+            color_with_alpha = (*self.color, alpha)
+            pygame.draw.circle(win, color_with_alpha, (int(scaled_x), int(scaled_y)), 1)
 
 def main():
     run = True
     clock = pygame.time.Clock()
 
-    sun = AstralBody(0, 0, 15, yellow, 1.98892e30)
+    sun = AstralBody(0, 0, 0, 30, yellow, 1.98892e30)
     sun.sun = True
-    
-    mercury = AstralBody(0.387 * AstralBody.astronomical_unit, 0, 8, white, 3.30e23)
+
+    mercury = AstralBody(0.387 * AstralBody.astronomical_unit, 0, 0, 8, white, 3.30e23)
     mercury.vy = -47400
 
-    venus = AstralBody(0.723 * AstralBody.astronomical_unit, 0, 14, green, 4.8685e24)
+    venus = AstralBody(0.723 * AstralBody.astronomical_unit, 0, 0, 14, green, 4.8685e24)
     venus.vy = -35020
 
-    earth = AstralBody(-1 * AstralBody.astronomical_unit, 0, 16, blue, 5.9742e24)
+    earth = AstralBody(-1 * AstralBody.astronomical_unit, 0, 0, 16, blue, 5.9742e24)
     earth.vy = 29783
 
-    mars = AstralBody(-1.524 * AstralBody.astronomical_unit, 0, 12, red, 6.39e23)
+    mars = AstralBody(-1.524 * AstralBody.astronomical_unit, 0, 0, 12, red, 6.39e23)
     mars.vy = 24077
 
     bodies = [sun, mercury, venus, earth, mars]
@@ -120,6 +128,9 @@ def main():
 
         for body in bodies:
             body.update_position(bodies)
+
+        for body in bodies:
+            body.draw_trail(window)
             body.draw(window)
 
         pygame.display.update()
